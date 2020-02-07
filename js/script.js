cbe0dd5dfcebaef83caaac16e3ca9d0e18129b62
Rahul <rahul.shinde@digitas.com>
2020-02-07 13:37:31 +0530

    Updated the json file

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 "use strict";
 
-fetch("./data.json")
-    .then(function(resp) {
-      return resp.json()
-    })
-    .then(function(data) {
-      console.log(data)
-    })
+
+// Fetch JSON data
+$.getJSON("https://raw.githubusercontent.com/shinderahul/json-manipulate/27936b74b933ad435cb76831047d8f37347c2f6c/js/data.json", function(response) {
+  manipulateData(response)
+});
+
+
+function manipulateData(response) {
+
+}
+
+
